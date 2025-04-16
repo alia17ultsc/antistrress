6014e00c2e0011debb3a3d0c437ce07fb89b85ae
alia17ultsc <aliafayez2009@gmail.com>
2025-04-16 18:31:03 +0200

    circles

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -25,8 +27,90 @@ class Mainbody extends StatefulWidget{
 }
 
 class _Mainbody extends State<Mainbody>{
+
+  List<Circle>circlesList=[];
+  double currentSize = 5;
+  var increaseSize=true;
+
+
+
+  void screenTap(TapDownDetails infoDetails){
+    var x=infoDetails.localPosition.dx;
+    var y=infoDetails.localPosition.dy;
+    print(x);
+    print(y);
+    var newcolor=Color.fromRGBO(Random().nextInt(256), Random().nextInt(256), Random().nextInt(256), 1);
+    var newCircle=Circle(position: infoDetails.localPosition, color: newcolor, circleSize: currentSize);
+    if(increaseSize==true){
+      currentSize+=3;
+      if(currentSize>100){
+        increaseSize=false;
+      }
+    }
+    else{
+      currentSize-=3;
+      if(currentSize<5){
+        increaseSize=true;
+      }
+    }
+    setState(() {
+       circlesList.add(newCircle);
+    });
+   
+  }
+
   @override
   Widget build(BuildContext context) {
-      return Text('mainbody');
+      return GestureDetector(
+        onTapDown:screenTap,
+        child: Stack(
+          children: [
+            Container(
+              width: double.infinity,
+              height: double.infinity,
+              color: const Color.fromARGB(255, 0, 0, 0),
+            ),
+            Positioned(
+              top: 3,
+              right: 3,
+              child: ElevatedButton(
+                onPressed: (){}, 
+                child: Text('Clear all'),
+                style: ElevatedButton.styleFrom(backgroundColor: const Color.fromARGB(255, 68, 224, 230)),
+                )),
+            Center(
+                child: Text(
+                  'Tap anywhere', 
+                  style: TextStyle(
+                    color: const Color.fromARGB(255, 60, 185, 165),
+                    fontSize: 20
+                    ),
+                  ),
+                ),
+                ...circlesList.map((eachCircle)=>Positioned(
+                  left: eachCircle.position.dx - eachCircle.circleSize/2,
+                  top: eachCircle.position.dy - eachCircle.circleSize/2,
+                  child: Container(
+                    width: eachCircle.circleSize,
+                    height: eachCircle.circleSize,
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: eachCircle.color
+                    ),
+                  )
+                )
+              )
+          ],
+
+          
+        )
+      );
   }
+}
+
+class Circle{
+  final Offset position;
+  final Color color;
+  final double circleSize;
+  Circle ({required this.position, required this.color, required this.circleSize});
 }
