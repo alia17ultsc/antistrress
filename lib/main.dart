5c77da9e31a68c6160d80fc7c7cc8d8210d98f2a
alia17ultsc <aliafayez2009@gmail.com>
2025-04-30 18:20:29 +0300

    finished project

--- lib/main.dart
+++ lib/main.dart
@@ -31,8 +31,14 @@ class _Mainbody extends State<Mainbody>{
   List<Circle>circlesList=[];
   double currentSize = 5;
   var increaseSize=true;
+  var staticSize=false;
+  var STbtn="D";
 
-
+  void clearCircles(){
+    setState(() {
+      circlesList.clear();
+    });
+  }
 
   void screenTap(TapDownDetails infoDetails){
     var x=infoDetails.localPosition.dx;
@@ -41,16 +47,20 @@ class _Mainbody extends State<Mainbody>{
     print(y);
     var newcolor=Color.fromRGBO(Random().nextInt(256), Random().nextInt(256), Random().nextInt(256), 1);
     var newCircle=Circle(position: infoDetails.localPosition, color: newcolor, circleSize: currentSize);
-    if(increaseSize==true){
-      currentSize+=3;
-      if(currentSize>100){
-        increaseSize=false;
+    if(STbtn == 'D'){
+      if(increaseSize==true){
+        currentSize+=3;
+        if(currentSize>=100){
+          currentSize = 100;
+          increaseSize=false;
+        }
       }
-    }
-    else{
-      currentSize-=3;
-      if(currentSize<5){
-        increaseSize=true;
+      else{
+        currentSize-=3;
+        if(currentSize<=5){
+          currentSize = 5;
+          increaseSize=true;
+        }
       }
     }
     setState(() {
@@ -59,35 +69,30 @@ class _Mainbody extends State<Mainbody>{
    
   }
 
+  void changeMode(){
+    setState(() {
+      if(STbtn == 'D'){
+        STbtn = 'S';
+      }
+      else{
+        STbtn = 'D';
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
       return GestureDetector(
         onTapDown:screenTap,
         child: Stack(
           children: [
+
             Container(
               width: double.infinity,
               height: double.infinity,
               color: const Color.fromARGB(255, 0, 0, 0),
             ),
-            Positioned(
-              top: 3,
-              right: 3,
-              child: ElevatedButton(
-                onPressed: (){}, 
-                child: Text('Clear all'),
-                style: ElevatedButton.styleFrom(backgroundColor: const Color.fromARGB(255, 68, 224, 230)),
-                )),
-            Center(
-                child: Text(
-                  'Tap anywhere', 
-                  style: TextStyle(
-                    color: const Color.fromARGB(255, 60, 185, 165),
-                    fontSize: 20
-                    ),
-                  ),
-                ),
-                ...circlesList.map((eachCircle)=>Positioned(
+              ...circlesList.map((eachCircle)=>Positioned(
                   left: eachCircle.position.dx - eachCircle.circleSize/2,
                   top: eachCircle.position.dy - eachCircle.circleSize/2,
                   child: Container(
@@ -99,7 +104,51 @@ class _Mainbody extends State<Mainbody>{
                     ),
                   )
                 )
-              )
+              ),
+              Positioned(
+                bottom: 60,
+                right: 10,
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(backgroundColor: const Color.fromARGB(125, 102, 73, 230)),
+                  onPressed: changeMode, 
+                  child: Text(
+                    '$STbtn',
+                    style: TextStyle(fontSize: 15, color: Colors.white),
+                    )
+                  )
+                ),
+            Positioned(
+              top: 35,
+              right: 8,
+              child: ElevatedButton(
+                onPressed: clearCircles, 
+                child: Text('Clear all'),
+                style: ElevatedButton.styleFrom(backgroundColor: const Color.fromARGB(151, 68, 225, 230)),
+                )),
+            Center(
+                child: Text(
+                  'Tap anywhere', 
+                  style: TextStyle(
+                    color: const Color.fromARGB(255, 60, 185, 165),
+                    fontSize: 20
+                    ), 
+                  ),
+                ),
+            Positioned(
+              left: 3,
+              bottom: 60,
+              child: Slider(
+                value: currentSize,
+                min: 5,
+                max: 100,
+                onChanged: (size){
+              setState(() {
+                currentSize = size;
+              });
+              
+             })
+              ),
+              
           ],
 
           
